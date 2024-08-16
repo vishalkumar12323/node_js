204c09a65c9cd4aa2011f2a17bda540f3b8d24d4
vishal <vishalninaniya8949info@gmail.com>
2024-08-16 12:42:15 +0530

    new subject node:streams

--- File-System/copy-dir/app.js
+++ File-System/copy-dir/app.js
@@ -1,16 +1,44 @@
 const fs = require("fs");
-const EventEmmiter = require("events");
+const Event = require("events");
+const path = require("path");
 
+const event = new Event();
 
+const inputDir = path.join(__dirname, 'input');
+const outputDir = path.join(__dirname, 'output');
 
-const event = new EventEmmiter();
+console.log('out dir ', outputDir);
+const copyDir = (src, destination) => {
+    if (!fs.existsSync(destination)) {
+        console.log('run block');
+        fs.mkdirSync(src, { recursive: true });
+    };
+    fs.readdirSync(src, { recursive: true }).forEach(item => {
+        const srcPath = path.join(src, item);
+        const destPath = path.join(destination, item);
+        // console.log('srcPath ', srcPath);
+        // console.log('dest path ', destPath);
+        if (fs.lstatSync(srcPath).isDirectory()) {
+            copyDir(srcPath, destPath);
+        } else if (fs.lstatSync(srcPath).isFile()) {
+            fs.copyFileSync(srcPath, destPath);
+        }
+    });
+};
+
 
 event.on("change", () => {
-    console.log('run.')
+    // fs.rmSync(outputDir, { recursive: true, force: true });
+    copyDir(inputDir, outputDir);
 });
 
 const init = () => {
-    const dir = fs.watch("./input", { recursive: true });
+    fs.watch("./input", { recursive: true }, (eventType, filename) => {
+        if (filename) {
+            event.emit("change");
+        }
+    });
 };
 
+copyDir('./input', './output');
 init();
