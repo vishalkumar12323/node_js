91dd2a6b2b1d464494e623d25d3292b14a1087d9
vishal <vishalninaniya8949info@gmail.com>
2024-08-17 18:18:41 +0530

    add readable stream

---
+++ Node-Streams/copy/app.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs/promises');
+
+(async () => {
+    console.time("T")
+    const srcFile = await fs.open("read.txt", 'r');
+    const destFile = await fs.open('copied.txt', 'w');
+
+    let readByte = -1
+    while (readByte !== 0) {
+        const result = await srcFile.read();
+        readByte = result.bytesRead;
+        if (readByte !== 16384) {
+            const indexNotfilled = result.buffer.indexOf(0);
+            const newBuffer = Buffer.alloc(indexNotfilled);
+            await destFile.write(newBuffer, 0, 0, indexNotfilled);
+        } else {
+            await destFile.write(result.buffer);
+        }
+    }
+    console.timeEnd("T");
+})();
---
+++ Node-Streams/http-stream/index.js
@@ -0,0 +1,22 @@
+const { createServer } = require("node:http");
+const fs = require("node:fs/promises");
+
+const server = createServer(async (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    const fileHandler = await fs.open('index.html', 'r');
+    const readStream = fileHandler.createReadStream();
+    readStream.on("data", (chunk) => {
+        data = chunk.toString("utf-8");
+        res.end(data);
+    });
+
+    // readStream.on("end", async () => {
+    //     await fileHandler.close();
+    // });
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening on 127.0.0.1:3000');
+});
