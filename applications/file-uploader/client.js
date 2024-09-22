2fae9adc2484cf0e954999a7775af1e016db2c98
vishal <vishalninaniya8949info@gmail.com>
2024-09-22 23:16:01 +0530

    update file-uploader app

--- applications/file-uploader/client.js
+++ applications/file-uploader/client.js
@@ -1,32 +1,61 @@
 const net = require("node:net");
 const fs = require("node:fs/promises");
+const path = require("path");
 
-const socket = net.createConnection({ port: 4050, host: "::1" }, () => {
-  console.log("connected to the server");
-});
-
-async function init() {
-  const fileHandler = await fs.open("one.txt", "r");
-  const stream = fileHandler.createReadStream({ encoding: "utf-8" });
-  let i = 0;
-  try {
-    stream.on("data", (data) => {
-      if (!socket.write(data)) {
-        stream.pause();
-      }
+async function moveCursor(dx, dy) {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
     });
-    socket.on("drain", () => {
-      console.log("drained! ", i++);
-      stream.resume();
-    });
-
-    stream.on("", () => {
-      console.log("file uploaded completed");
-      // socket.end();
-    });
-  } catch (err) {
-    console.log("err ", err);
-  }
+  });
 }
 
-init();
+async function clearLine(direction) {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(direction, () => {
+      resolve();
+    });
+  });
+}
+const socket = net.createConnection(
+  { port: 4050, host: "::1", family: "IPv6" },
+  async () => {
+    const filePath = process.argv[2];
+    const fileName = path.basename(filePath);
+
+    const fileHandler = await fs.open(filePath, "r");
+    const readFileStream = fileHandler.createReadStream();
+
+    const fileSize = (await fileHandler.stat()).size;
+
+    let bytesUploaded = 0,
+      uploadedPercentage = 0;
+    socket.write(`fileName ${fileName}---`);
+
+    console.log();
+
+    readFileStream.on("data", async (data) => {
+      if (!socket.write(data)) {
+        readFileStream.pause();
+      }
+      bytesUploaded += data.length;
+      let newPercentage = Math.floor((bytesUploaded / fileSize) * 100);
+      if (newPercentage !== uploadedPercentage) {
+        uploadedPercentage = newPercentage;
+
+        await moveCursor(0, -1);
+        await clearLine(0);
+        console.log(`file uploading...${uploadedPercentage}%`);
+      }
+    });
+
+    socket.on("drain", () => {
+      readFileStream.resume();
+    });
+
+    readFileStream.on("end", () => {
+      console.log("The file was successfully uploaded!");
+      socket.end();
+    });
+  }
+);
--- applications/file-uploader/index.js
+++ applications/file-uploader/index.js
@@ -3,47 +3,44 @@ const fs = require("node:fs/promises");
 
 const PORT = 4050,
   HOSTNAME = "::1"; // IPv6
-const server = net.createServer();
-server.on("connection", (client) => {
+const server = net.createServer(() => {});
+server.on("connection", (socket) => {
   console.log("a new connection to the server");
-  let fileHandler,
-    stream,
-    i = 0;
-  client.on("data", async (data) => {
-    try {
-      if (!fileHandler) {
-        client.pause();
-        fileHandler = await fs.open("file.txt", "w");
-        stream = fileHandler.createWriteStream({ encoding: "utf-8" });
-        stream.write(data);
+  let fileHandler, stream;
+  socket.on("data", async (data) => {
+    if (!fileHandler) {
+      socket.pause();
 
-        client.resume();
-        stream.on("drain", () => {
-          console.log("drained! ", i++);
-          client.resume(); // if writable stream internal buffer is empty then resume reading data from the client
-        });
-      } else {
-        if (!stream.write(data)) {
-          client.pause(); // if writable stream internal buffer is full then pause reading data from the client
-        }
+      const indexOfDivider = data.indexOf("---");
+      const fileName = data.subarray(9, indexOfDivider).toString("utf-8");
+
+      fileHandler = await fs.open(`storage/${fileName}`, "w");
+      stream = fileHandler.createWriteStream();
+
+      stream.write(data.subarray(indexOfDivider + 3));
+      socket.resume();
+
+      stream.on("drain", () => {
+        socket.resume(); // if writable stream internal buffer is empty then resume reading data from the socket
+      });
+    } else {
+      if (!stream.write(data)) {
+        socket.pause(); // if writable stream internal buffer is full then pause reading data from the socket
       }
-    } catch (err) {
-      console.log("error ", err);
     }
-
-    stream.on("finish", async () => {
-      await fileHandler.close();
-      console.log("data reading finish.");
-    });
   });
 
-  client.on("end", () => {
+  socket.on("end", async () => {
+    if (fileHandler) await fileHandler.close();
     fileHandler = null;
     stream = null;
-    console.log("completed data reading from the client-side.");
-  });
-  client.on("error", () => {
     console.log("connection ended.");
+  });
+  socket.on("error", async () => {
+    // if (fileHandler) await fileHandler.close();
+    // fileHandler = null;
+    // stream = null;
+    // console.log("connection ended.");
   });
 });
 server.listen(PORT, HOSTNAME, () =>
