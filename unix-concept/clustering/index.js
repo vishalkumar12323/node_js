a625d5a5d78196292cce06f98723f460e8a52d52
vishal <vishalninaniya8949info@gmail.com>
2024-10-13 23:03:26 +0530

    learing node-clustering

--- unix-concept/clustering/index.js
+++ unix-concept/clustering/index.js
@@ -1,13 +1,14 @@
 const cpeak = require("cpeak");
 
+const port = 4050;
 const app = new cpeak();
 
 app.route("get", "/", (req, res) => {
   res.json({ msg: "Hello baddy." });
 });
 app.route("get", "/havy", (req, res) => {
-  for (let i = 0; i < 90000000000; i++) {}
+  // for (let i = 0; i < 90000000000; i++) {}
   res.json({ msg: "This the havy operation." });
 });
 
-app.listen(4040, () => console.log("server opened on port 4040"));
+app.listen(port, () => console.log(`server opened on port:${port}`));
